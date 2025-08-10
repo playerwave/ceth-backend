1f9527be4097a95012b590d887c1716c9217c0c9
Thanaphat Ketsani <65160169@go.buu.ac.th>
2025-08-10 12:24:04 +0700

    fix: change db link & debug question.entity

--- src/entity/question.entity.ts
+++ src/entity/question.entity.ts
@@ -1,35 +1,51 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
-import { SetNumber } from './setNumbers.entity';
-import { Choice } from './choice.entity';
-import { Answer } from './answer.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  OneToMany,
+} from "typeorm";
+import { SetNumber } from "./setNumbers.entity";
+import { Choice } from "./choice.entity";
+import { Answer } from "./answer.entity";
 @Entity()
 export class Question {
-    @PrimaryGeneratedColumn()
-    question_id!: number;
+  @PrimaryGeneratedColumn()
+  question_id!: number;
 
-    @Column({ type: 'text' })
-    question_text!: string;
+  @Column({ type: "text" })
+  question_text!: string;
 
-    @Column({ type: 'int' })
-    question_number?: number;
+  @Column({ type: "int" })
+  question_number?: number;
 
-    @ManyToOne(() => SetNumber, (setNumber) => setNumber.question)
-    @JoinColumn({ name: 'set_number_id' })
-    setNumber?: SetNumber;
+  @ManyToOne(() => SetNumber, (setNumber) => setNumber.question)
+  @JoinColumn({ name: "set_number_id" })
+  setNumber?: SetNumber;
 
-    @Column()
-    set_number_id?: number;
+  @Column()
+  set_number_id?: number;
 
-    @Column({
-        type: 'enum',
-        enum: ['Fix Single answer', 'Single answer', 'Multiple answer', 'Text answer'],
-        default: 'Text answer'
-    })
-    question_type?: 'Fix Single answer' | 'Single answer' | 'Multiple answer' | 'Text answer';
+  @Column({
+    type: "enum",
+    enum: [
+      "Fix Single answer",
+      "Single answer",
+      "Multiple answer",
+      "Text answer",
+    ],
+    default: "Text answer",
+  })
+  question_type?:
+    | "Fix Single answer"
+    | "Single answer"
+    | "Multiple answer"
+    | "Text answer";
 
-    @OneToMany(() => Choice, (choice) => choice.question)
-    choice?: Choice[];
+  @OneToMany(() => Choice, (choice) => choice.question)
+  choice?: Choice[];
 
-    @OneToMany(() => Answer, (answer) => answer.question)
-    answer?: Answer[];
+  @OneToMany(() => Answer, (answer) => answer.question)
+  answer?: Answer[];
 }
